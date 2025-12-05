534550a1666c1393eb7a89d13f7a6227b708b678
sourav0809 <spathak7431@gmail.com>
2025-12-05 11:03:50 +0530

    refactor: Update Next.js configuration to allow remote image loading from any source. Simplified remotePatterns by using wildcard hostnames for both HTTPS and HTTP protocols.

--- next.config.ts
+++ next.config.ts
@@ -4,28 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
+        protocol: "https",
+        hostname: "**",
       },
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'graph.facebook.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'platform-lookaside.fbsbx.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.auth0.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.googleusercontent.com',
+        protocol: "http",
+        hostname: "**",
       },
     ],
   },
